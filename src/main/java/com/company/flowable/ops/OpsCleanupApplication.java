c44d313d33cb30391145accec04853c6b2ea870a
jsoomro <jalilsoomro@gmail.com>
2026-01-14 03:18:13 +0400

    updated OpsCleanupApplication.java to fix startup issue after FlowableEngineConfig changes.

--- src/main/java/com/company/flowable/ops/OpsCleanupApplication.java
+++ src/main/java/com/company/flowable/ops/OpsCleanupApplication.java
@@ -3,12 +3,14 @@ package com.company.flowable.ops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableConfigurationProperties({OpsCleanupProperties.class, OpsSecurityProperties.class})
+@Import(FlowableEngineConfig.class)
 public class OpsCleanupApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpsCleanupApplication.class, args);
